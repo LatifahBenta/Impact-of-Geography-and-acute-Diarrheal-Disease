ae13ddd287e47b911fa4c31f21c9ffb2f8cc0682
LatifahBenta <102129180+LatifahBenta@users.noreply.github.com>
2024-10-15 17:14:44 +0300

    Update trimmomatic_script.sh

--- Scripts/trimmomatic_script.sh
+++ Scripts/trimmomatic_script.sh
@@ -22,10 +22,12 @@ for forward_read in "$data_folder"/*_1.fastq; do
 
     # Define the path for the trimmed output
     trimmed_forward="${output_dir}/${dataset}_1_trimmed.fastq"
-    trimmed_reverse="${output_dir}/${dataset}_2_trimmed.fastq"
+    trimmed_reverse="${output_dir}/${dataset}_2_trimmeCreated.fastq"
 
     # Run Trimmomatic with TruSeq adapter file
     trimmomatic PE -phred33 "$forward_read" "$reverse_read" \
+
+    ### My name
         "$trimmed_forward" "$output_dir/unpaired_${dataset}_1.fastq" \
         "$trimmed_reverse" "$output_dir/unpaired_${dataset}_2.fastq" \
         ILLUMINACLIP:"$adapter_file":2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:4:30 MINLEN:36
